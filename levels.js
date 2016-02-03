d2567bcdb608b4de1f3d43260bac3fe2181925ff
FTWinston <ftwinston@gmail.com>
2016-02-03 14:30:56 +0000

    Added an "intro" before the first level is shown, and split the level data out into discrete objects

---
+++ levels.js
@@ -0,0 +1,27 @@
+levels.push(new Level({
+	timeSigBeats: 4,
+	timeSigValue: 4,
+	tempoNote: '4',
+	tempoBeats: 90,
+	warmupTime: 15,
+	bars: [
+		['4', '4', '4r', '4'],
+		['4', '4r', '4r', '4'],
+		['4r', '4', '4', '4r'],
+		['4r', '4', '4', '4']
+	]
+}));
+
+levels.push(new Level({
+	timeSigBeats: 4,
+	timeSigValue: 4,
+	tempoNote: '4',
+	tempoBeats: 80,
+	warmupTime: 10,
+	bars: [
+		['8', '8', '4r', '4', '4'],
+		['8', '8', '4r', '4', '4'],
+		['8', '8', '4r', '4', '4'],
+		['8', '8', '8', '8', '8', '8', '4'],
+	]
+}));
--- subdivide.js
+++ subdivide.js
@@ -17,8 +17,12 @@ function init() {
 			pressed = false;
 	};
 	
-	var render = document.getElementById('render');
-	render.addEventListener('touchstart', function(e) {
+	document.getElementById('render').addEventListener('touchstart', function(e) {
+		beatInput();
+		e.preventDefault();
+	}, false);
+	
+	document.getElementById('intro').addEventListener('touchstart', function(e) {
 		beatInput();
 		e.preventDefault();
 	}, false);
@@ -31,35 +35,88 @@ function handleResize(value, e) {
 	draw();
 }
 
-var bars = [
-	['q', 'q', 'qr', 'q'],
-	['q', 'qr', 'qr', 'q'],
-	['qr', 'q', 'q', 'qr'],
-	['qr', 'q', 'q', 'q']
-];
-
-var timeSigBeats = 4, timeSigValue = 4;
-
 function draw() {
+	if (currentLevel == null)
+		return;
+	
 	var render = document.getElementById('render');
-	var width = parseInt(render.getAttribute('width'));
-	var inset = width * 0.02, staveWidth = width - inset * 2;
-	
-	var renderer = new Vex.Flow.Renderer(render, Vex.Flow.Renderer.Backends.CANVAS);
-	var ctx = renderer.getContext();
-	
-	// add initial "bar" just for clef & time signature
-	var bar = drawBar(ctx, timeSigBeats, timeSigValue, undefined, inset, 75, true, false);
-	staveWidth -= bar.width;
-	
-	for (var i=0; i<bars.length; i++) {
-		var notes = parseNotes(bars[i]);
-		var x = bar.width + bar.x;
-		bar = drawBar(ctx, timeSigBeats, timeSigValue, notes, x, staveWidth / bars.length, false, i == bars.length - 1);
+	currentLevel.draw(render);
+}
+
+var GameState = {
+	NoLevel: 0,
+	Warmup: 1,
+	Active: 2,
+	Finished: 3,
+}
+
+var levelNum = 0, gameState = GameState.NoLevel;
+var levels = [], currentLevel = null;
+
+function beatInput() {
+	switch(gameState) {
+		case GameState.NoLevel:
+		case GameState.Finished:
+			moveToLevel(levelNum + 1);
+			return;
+		case GameState.Warmup:
+			startLevel();
+			return;
+		case GameState.Active:
+			drawBeat();
+			return;
 	}
 }
 
-function parseNotes(input) {
+function moveToLevel(num) {
+	levelNum = num;
+	gameState = GameState.Warmup;
+	
+	document.getElementById('intro').setAttribute('style', 'display:none;');
+	document.getElementById('game').setAttribute('style', '');
+	document.getElementById('levelNum').innerHTML = levelNum;
+	
+	document.getElementById('beatIndicator').setAttribute('style', 'display:none;');
+	document.getElementById('countdown').setAttribute('style', '');
+	
+	currentLevel = levels[levelNum - 1];
+	draw();
+}
+
+function startLevel() {
+	gameState = GameState.Active;
+	
+	document.getElementById('countdown').setAttribute('style', 'display:none;');
+	document.getElementById('beatIndicator').setAttribute('style', '');
+}
+
+function Level(data) {
+	this.timeSigBeats = data.timeSigBeats;
+	this.timeSigValue = data.timeSigValue;
+	this.tempoNote = data.tempoNote;
+	this.tempoBeats = data.tempoBeats;
+	this.bars = data.bars;
+}
+
+Level.prototype.draw = function(canvas) {
+	var width = parseInt(canvas.getAttribute('width'));
+	var inset = width * 0.02, staveWidth = width - inset * 2;
+	
+	var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
+	var ctx = renderer.getContext();
+	
+	// add initial "bar" just for clef & time signature
+	var bar = this.drawBar(ctx, [], inset, 75, true, false);
+	staveWidth -= bar.width;
+	
+	for (var i=0; i<this.bars.length; i++) {
+		var notes = this.parseNotes(this.bars[i]);
+		var x = bar.width + bar.x;
+		bar = this.drawBar(ctx, notes, x, staveWidth / this.bars.length, false, i == this.bars.length - 1);
+	}
+};
+
+Level.prototype.parseNotes = function(input) {
 	var output = []
 	for (var i=0; i<input.length; i++) {
 		var val = input[i];
@@ -69,41 +126,70 @@ function parseNotes(input) {
 	return output;
 }
 
-function drawBar(ctx, numBeats, beatValue, notes, startX, barWidth, firstBar, lastBar) {
+Level.prototype.drawBar = function(ctx, notes, startX, barWidth, firstBar, lastBar) {
 	var stave = new Vex.Flow.Stave(startX, 0, barWidth);
 	
 	if (firstBar) {
 		stave.addClef('treble');
-		stave.addTimeSignature(numBeats + '/' + beatValue);
+		stave.addTimeSignature(this.timeSigBeats + '/' + this.timeSigValue);
+		stave.setTempo({duration: this.tempoNote, bpm: this.tempoBeats}, 10);
 	}
 	stave.setBegBarType(firstBar ? Vex.Flow.Barline.type.SINGLE : Vex.Flow.Barline.type.NONE);
 	stave.setEndBarType(lastBar ? Vex.Flow.Barline.type.END : firstBar ? Vex.Flow.Barline.type.NONE : Vex.Flow.Barline.type.SINGLE);
 	stave.setContext(ctx).draw();
 	
 	var voice = new Vex.Flow.Voice({
-		num_beats: numBeats,
-		beat_value: beatValue,
+		num_beats: this.timeSigBeats,
+		beat_value: this.timeSigValue,
 		resolution: Vex.Flow.RESOLUTION
 	});
 	
-	if (firstBar)
-		voice.mode = Vex.Flow.Voice.Mode.SOFT;
-	else
-		voice.addTickables(notes);
+	voice.mode = Vex.Flow.Voice.Mode.SOFT;
+	voice.addTickables(notes);
 
 	var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], barWidth * 0.95);
-
-	voice.draw(ctx, stave);
-	
 	var beams = Vex.Flow.Beam.applyAndGetBeams(voice);
+	
+	voice.draw(ctx, stave);
 	
 	beams.forEach(function(beam) {
 		beam.setContext(ctx).draw();
 	});
 	
 	return stave;
+};
+
+var beatFadeStart = undefined, beatFadeDuration = 1000, fade = undefined;
+
+function drawBeat() {
+	var ctx = document.getElementById('beatIndicator').getContext('2d');
+	ctx.strokeStyle = '#00cc00';
+	ctx.lineWidth = 10;
+	ctx.beginPath();
+	ctx.arc(40, 40, 30, 0, Math.PI * 2);
+	ctx.stroke();
+	
+	if (fade !== undefined)
+		window.cancelAnimationFrame(fade);
+	
+	beatFadeStart = performance.now();
+	fade = window.requestAnimationFrame(fadeBeat);
 }
 
-function beatInput() {
-	console.log('beat');
+function fadeBeat(timestamp) {
+	var progress = timestamp - beatFadeStart;
+
+	// overdraw with opacity
+	var ctx = document.getElementById('beatIndicator').getContext('2d');
+	ctx.fillStyle = '#ffffff';
+	ctx.globalAlpha = 0.15;
+	ctx.beginPath();
+	ctx.rect(0, 0, 80, 80);
+	ctx.fill();
+	ctx.globalAlpha = 1;
+
+	if (progress < beatFadeDuration)
+		fade = window.requestAnimationFrame(fadeBeat);
+	else
+		fade = beatFadeStart = undefined;
 }
