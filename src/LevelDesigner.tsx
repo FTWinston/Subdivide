a6227a19535aaa95b7c2cf7d06de25a00cf25446
FTWinston <ftwinston@gmail.com>
2018-10-22 14:33:45 +0100

    Designer no longer lets you add beats that are longer than the remaining time in the bar

--- src/LevelDesigner.tsx
+++ src/LevelDesigner.tsx
@@ -12,6 +12,7 @@ interface IProps {
 
 interface IState extends IMusic {
     navigateUrl?: string;
+    remainingTicksInBar: number;
 }
 
 export class LevelDesigner extends React.PureComponent<IProps, IState> {
@@ -22,6 +23,7 @@ export class LevelDesigner extends React.PureComponent<IProps, IState> {
             this.state = {
                 bars: [[]],
                 name: 'Custom rhythm',
+                remainingTicksInBar: 4 * NoteLength.Crotchet,
                 tempo: [NoteLength.Crotchet, 80],
                 timeSignature: [4, NoteLength.Crotchet],
             };
@@ -31,6 +33,7 @@ export class LevelDesigner extends React.PureComponent<IProps, IState> {
             this.state = {
                 bars: level.bars,
                 name: level.name,
+                remainingTicksInBar: level.timeSignature[0] * level.timeSignature[1],
                 tempo: level.tempo,
                 timeSignature: level.timeSignature,
             }
@@ -44,12 +47,24 @@ export class LevelDesigner extends React.PureComponent<IProps, IState> {
 
         const timeSigBeatNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
             .map(i => <option key={i} value={i}>{i}</option>);
-        const timeSigNumberChanged = (e: React.ChangeEvent<HTMLSelectElement>) => this.setState({
-            timeSignature: [parseInt(e.target.value, 10), this.state.timeSignature[1]],
-        });
-        const timeSigLengthChanged = (e: React.ChangeEvent<HTMLSelectElement>) => this.setState({
-            timeSignature: [this.state.timeSignature[0], parseInt(e.target.value, 10)],
-        });
+            
+        const timeSigNumberChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
+            const newVal = parseInt(e.target.value, 10);
+
+            this.setState({
+                remainingTicksInBar: newVal * this.state.timeSignature[1],
+                timeSignature: [newVal, this.state.timeSignature[1]],
+            });
+        }
+
+        const timeSigLengthChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
+            const newVal = parseInt(e.target.value, 10);
+            
+            this.setState({
+                remainingTicksInBar: this.state.timeSignature[0] * newVal,
+                timeSignature: [this.state.timeSignature[0], newVal],
+            });
+        }
         
         const tempoNumberChanged = (e: React.ChangeEvent<HTMLInputElement>) => this.setState({
             tempo: [this.state.tempo[0], parseInt(e.target.value, 10)],
@@ -146,44 +161,53 @@ export class LevelDesigner extends React.PureComponent<IProps, IState> {
         lastBar.push({
             length,
             type,
-        })
+        });
 
+        const remainingTicks = this.state.remainingTicksInBar - length
+        
         this.setState({
-            bars
-        })
+            bars,
+            remainingTicksInBar: remainingTicks <= 0
+                ? this.state.timeSignature[0] * this.state.timeSignature[1]
+                : remainingTicks,
+        });
     }
     
     private renderNoteButtons(name: string, normalLength: NoteLength, dottedLength?: NoteLength, tripletLength?: NoteLength): any {
-        const addNormal = () => this.addNote(normalLength, NoteType.Note);
-        const addNormalRest = () => this.addNote(normalLength, NoteType.Rest);
+
+        const addNormal = normalLength <= this.state.remainingTicksInBar ? () => this.addNote(normalLength, NoteType.Note) : undefined;
+        const addNormalRest = normalLength <= this.state.remainingTicksInBar ? () => this.addNote(normalLength, NoteType.Rest) : undefined;
         
-        const addDotted = () => this.addNote(dottedLength!, NoteType.Note);
-        const addDottedRest = () => this.addNote(dottedLength!, NoteType.Rest);
+        let dottedGroup: JSX.Element | undefined;
+        let tripletGroup: JSX.Element | undefined;
 
-        const addTriplet = () => this.addNote(tripletLength!, NoteType.Note);
-        const addTripletRest = () => this.addNote(tripletLength!, NoteType.Rest);
+        if (dottedLength !== undefined) {
+            const addDotted = dottedLength <= this.state.remainingTicksInBar ? () => this.addNote(dottedLength, NoteType.Note) : undefined;
+            const addDottedRest = dottedLength <= this.state.remainingTicksInBar ? () => this.addNote(dottedLength, NoteType.Rest) : undefined;
 
-        const dottedGroup = dottedLength === undefined
-             ? undefined
-             : <div className="noteButtons__lengthGroup">
+            dottedGroup = <div className="noteButtons__lengthGroup">
                 <div className="noteButtons__label">Dotted {name.toLowerCase()}</div>
-                 <button onClick={addDotted}>Note</button>
-                 <button onClick={addDottedRest}>Rest</button>
-             </div>
+                <button onClick={addDotted} disabled={addDotted === undefined}>Note</button>
+                <button onClick={addDottedRest} disabled={addDottedRest === undefined}>Rest</button>
+            </div>
+        }
 
-        const tripletGroup = tripletLength === undefined
-             ? undefined
-             : <div className="noteButtons__lengthGroup">
+        if (tripletLength !== undefined) {
+            const addTriplet = tripletLength <= this.state.remainingTicksInBar ? () => this.addNote(tripletLength, NoteType.Note) : undefined;
+            const addTripletRest = tripletLength <= this.state.remainingTicksInBar ? () => this.addNote(tripletLength, NoteType.Rest) : undefined;
+
+            tripletGroup = <div className="noteButtons__lengthGroup">
                 <div className="noteButtons__label">Triplet {name.toLowerCase()}</div>
-                 <button onClick={addTriplet}>Note</button>
-                 <button onClick={addTripletRest}>Rest</button>
-             </div>
+                <button onClick={addTriplet} disabled={addTriplet === undefined}>Note</button>
+                <button onClick={addTripletRest} disabled={addTripletRest === undefined}>Rest</button>
+            </div>
+        }
 
         return <div className="noteButtons">
             <div className="noteButtons__lengthGroup noteButtons__lengthGroup--main">
                 <div className="noteButtons__label">{name}</div>
-                <button onClick={addNormal}>Note</button>
-                <button onClick={addNormalRest}>Rest</button>
+                <button onClick={addNormal} disabled={addNormal === undefined}>Note</button>
+                <button onClick={addNormalRest} disabled={addNormalRest === undefined}>Rest</button>
             </div>
             {dottedGroup}
             {tripletGroup}
